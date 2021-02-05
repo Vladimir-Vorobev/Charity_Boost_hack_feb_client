afcb4ffb2f0687ea12cc70d4429c182a18403195
SerPan23 <igracom@yandex.ru>
2021-02-05 22:00:28 +0300

    u

---
+++ src/components/market.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+    <main>
+        <div class="row justify-content-around">
+            <div class="col-4 col-lg-2">
+                <h3>Помочь:</h3>
+            </div>
+        </div>
+    </main>
+</div>
+
+</template>
+
+
+<script>
+export default {
+    name: 'market',
+}
+</script>
+
+<style scoped>
+main {
+    padding-top: 70px;
+}
+footer {
+    background-color: #ffffff;
+    padding: 0px;
+}
+</style>
