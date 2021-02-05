4e9ab448852f65eebda351e52b136acb1fac79ba
SerPan23 <igracom@yandex.ru>
2021-02-05 21:54:25 +0300

    u

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import App from './App.vue'
 import main_page from './components/main_page.vue'
 import login from './components/login.vue'
 import registration from './components/registration.vue'
+import projects from './components/projects.vue'
 
 Vue.config.productionTip = false
 Vue.use(VueRouter)
@@ -16,6 +17,7 @@ const routes = [
   { path: '/', component: main_page },
   { path: '/login', component: login },
   { path: '/registration', component: registration },
+  { path: '/projects', component: projects },
 ]
 
 const router = new VueRouter({
