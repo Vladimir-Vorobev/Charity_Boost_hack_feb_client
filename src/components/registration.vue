9977e80e9f930d656b66e97a60ab8ca140f5d2be
SerPan23 <igracom@yandex.ru>
2021-02-06 15:09:45 +0300

    Update registration.vue

--- src/components/registration.vue
+++ src/components/registration.vue
@@ -68,7 +68,60 @@
                 </form> <br>
             </div>
             <div class="formbox mb-5" v-else-if="type_reg == 1">
-
+                <form>
+                    <h2 class="text-center">Форма регистрации для бизнеса</h2>
+                    <div class="text-left" v-if="reg == 0">Пожалуйста, заполните форму и внимательно ознакомьтесь с Условиями пользования платформой. Поля со звездочкой(<span class="star">*</span>) обязательны к заполнению.</div><br>
+                    <div v-if="reg == 0">
+                        <div class="form-group row">
+                            <label for="exampleInputEmail1">Название фонда/НКО<span class="star">*</span></label>
+                            <input type="email" name="name" class="form-control formInput">
+                        </div>
+                        <div class="form-group row">
+                            <label for="exampleInputEmail1">Регистрационный номер<span class="star">*</span></label>
+                            <input type="email" name="number" class="form-control formInput">
+                        </div>
+                        <div class="form-group row">
+                            <label for="exampleInputEmail1">Свидетельство о регистрации<span class="star">*</span></label>
+                            <input type="email" name="certificate" class="form-control formInput">
+                        </div>
+                        <div class="form-group row">
+                            <label for="exampleInputEmail1">Адрес электронной почты<span class="star">*</span> (для связи)</label>
+                            <input type="email" name="email" class="form-control formInput">
+                        </div>
+                        <div class="form-group row">
+                            <label for="exampleInputEmail1">Номер телефона<span class="star">*</span> (для связи)</label>
+                            <input type="email" name="phone" class="form-control formInput">
+                        </div>
+                        <div class="form-group row">
+                            <label for="exampleInputEmail1">Адрес<span class="star">*</span></label>
+                            <input type="email" name="address" class="form-control formInput">
+                        </div>
+                        <div class="form-group row">
+                            <label for="exampleInputEmail1">Ссылка на сайт</label>
+                            <input type="email" name="site" class="form-control formInput">
+                        </div>
+                        <div class="form-group row">
+                            <label for="exampleInputPassword1">Пароль<span class="star">*</span></label>
+                            <input type="password" class="form-control formInput" name="password">
+                        </div>
+                        <div class="form-group row">
+                            <label for="exampleInputPassword1">Повторите пароль<span class="star">*</span></label>
+                            <input type="password" class="form-control formInput" name="password2">
+                        </div>
+                        <div class="row"> 
+                            <button class="btn btn-rounded-blue btn-lg" @click="code()">Отправить</button>
+                        </div>
+                    </div>
+                    <div v-else>
+                        <div class="form-group row">
+                            <label for="exampleInputEmail1">Код подтверждения</label>
+                            <input name="code" class="form-control formInput">
+                        </div>
+                        <div class="row"> 
+                            <button class="btn btn-rounded-blue btn-lg" @click="registration()">Зарегистрироваться</button>
+                        </div>
+                    </div>
+                </form> <br>
             </div>
         </div>
         <div class="footer"><Footer></Footer></div> 
