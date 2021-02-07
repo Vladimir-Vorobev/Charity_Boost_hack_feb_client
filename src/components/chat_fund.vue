3fc4df2af2f2ba647607211c568b378102da527e
SerPan23 <igracom@yandex.ru>
2021-02-07 12:08:19 +0300

    Update chat_fund.vue

--- src/components/chat_fund.vue
+++ src/components/chat_fund.vue
@@ -2,103 +2,37 @@
 <div>
     <main>
         <div class="menu">
-            <a href="#" class="back"><i class="fa fa-angle-left"></i> </a>
             <div class="name">Chat name</div>
-            <div class="members"><b>You</b>, Marga, Charo & Brotons</div>
         </div>
         <ol class="chat">
-            <li class="other unread">
+            <li class="other">
                 <div class="msg">
-                    <div class="user">Marga<span class="range admin">Admin</span></div>
+                    <div class="user">Marga</div>
                     <p>Dude</p>
-                    <p>Want to go dinner? <emoji class="pizza"></emoji></p>
+                    <p>Want to go dinner?</p>
                     <time>20:17</time>
                 </div>
             </li>
             <li class="self">
                 <div class="msg">
                     <p>Puff...</p>
-                    <p>I'm still doing the Góngora comment... <emoji class="books"></emoji></p>
+                    <p>I'm still doing the Góngora comment...</p>
                     <p>Better other day</p>
                     <time>20:18</time>
                 </div>
             </li>
-            <li class="other">
-            <div class="msg">
-                <div class="user">Brotons</div>
-                <p>What comment about Góngora? <emoji class="suffocated"></emoji></p>
-                <time>20:18</time>
-            </div>
-            </li>
-            <li class="self">
-            <div class="msg">
-                <p>The comment sent Marialu</p>
-                <p>It's for tomorrow</p>
-                <time>20:18</time>
-            </div>
-            </li>
-            <li class="other">
-            <div class="msg">
-                <div class="user">Brotons</div>
-                <p><emoji class="scream"></emoji></p>
-                <p>Hand it to me! <emoji class="please"></emoji></p>
-                <time>20:18</time>
-            </div>
-            </li>
-            <li class="self">
-            <div class="msg">
-                <img src="https://i.imgur.com/kUPxcsI.jpg" draggable="false"/>
-                <time>20:19</time>
-            </div>
-            </li>
-            <li class="other">
-            <div class="msg">
-                <div class="user">Brotons</div>
-                <p>Thank you! <emoji class="hearth_blue"></emoji></p>
-                <time>20:20</time>
-            </div>
-            </li>
-                <div class="day">Today</div>
-            <li class="self">
-            <div class="msg">
-                <p>Who wants to play Minecraft?</p>
-                <time>18:03</time>
-            </div>
-            </li>
-            <li class="other">
-            <div class="msg">
-                <div class="user">Charo</div>
-                <p>Come on, I didn't play it for four months</p>
-                <time>18:07</time>
-            </div>
-            </li>
-            <li class="self">
-            <div class="msg">
-                <p>Ehh, the launcher crash... <emoji class="cryalot"></emoji></p>
-                <time>18:08</time>
-            </div>
-            </li>
-            <li class="other">
-            <div class="msg">
-                <div class="user">Charo</div>
-                <p><emoji class="lmao"></emoji></p>
-                <p>Sure that is the base code</p>
-                <p>I told it to Mojang</p>
-                <time>18:08</time>
-            </div>
-            </li>
-            <li class="self">
-            <div class="msg">
-                <p>It's a joke</p>
-                <p>Moai attack!</p>
-                <p><emoji class="moai"></emoji><emoji class="moai"></emoji><emoji class="moai"></emoji><emoji class="moai"></emoji><emoji class="moai"></emoji><emoji class="moai"></emoji></p>
-                <time>18:10</time>
-            </div>
+            <div class="day">Today</div>
+            <li class="other unread">
+                <div class="msg">
+                    <div class="user">Brotons</div>
+                    <p>What comment about Góngora?</p>
+                    <time>20:18</time>
+                </div>
             </li>
         </ol>
         <div class="typezone">
             <form><textarea type="text" placeholder="Say something"></textarea><input type="submit" class="send" value=""/></form>
-            <div class="emojis"></div>
+            <!-- <div class="emojis"></div> -->
         </div>
     </main>
 </div>
